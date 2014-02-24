d9d88480a6f7106bb2b80f5d0698c37ac268ff80
izissise <hugues.m@hotmail.fr>
2014-02-24 17:19:25 +0100

    Save and restore rdi in strlen

--- test/test_memset.c
+++ test/test_memset.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 int	main(int argc, char *argv[])
 {
+  char tmp[] = "tsmjkfrjsdlkfjpoer";
   char	*str;
 
-  str = strdup("tsmjkfrjsdlkfjpoer");
-  memset(str, 'A', strlen(str));
+  str = strdup(tmp);
   printf("str => %s | ", str);
+  memset(str, 'A', sizeof(tmp)  - 1);
   printf("str -> %s\n", str);
   free(str);
   return 0;
