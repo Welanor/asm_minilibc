7ff0f726cf1007eb21dc4b80fc0e9dbc18a49245
dellam_a <adrien.dellamaggiora@epitech.eu>
2014-02-24 14:51:18 +0100

    Finish strlen and add a main to test the function

---
+++ test/test_strlen.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+
+int	main(int argc, char *argv[])
+{
+  char	*str;
+
+  str = strdup("toto");
+  printf ("str => %s | %d\n", str, strlen(str));
+  free(str);
+  str = strdup("");
+  printf ("str => %s | %d\n", str, strlen(str));
+  free(str);
+  str = strdup("aaaaaaaaaaaaaaaaaaaaaaa");
+  printf ("str => %s | %d\n", str, strlen(str));
+  free(str);
+  str = strdup("aaa");
+  printf ("str => %s | %d\n", str, strlen(str));
+  free(str);
+  str = strdup("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+  printf ("str => %s | %d\n", str, strlen(str));
+  free(str);
+  str = strdup("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+  printf ("str => %s | %d\n", str, strlen(str));
+  free(str);
+  return 0;
+}
