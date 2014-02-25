bacd4588344d36756f6251b25290b2dff3227ec0
dellam_a <adrien.dellamaggiora@epitech.eu>
2014-02-25 20:51:24 +0100

    Add test for strchr function

---
+++ test/test_strchr.c
@@ -0,0 +1,14 @@
+int main(int argc, char *argv[])
+{
+  printf("%s\n", strchr("azertyuiop", 'a'));
+  printf("%s\n", strchr("azertyuiop", 'z'));
+  printf("%s\n", strchr("azertyuiop", 'e'));
+  printf("%s\n", strchr("azertyuiop", 'r'));
+  printf("%s\n", strchr("azertyuiop", 't'));
+  printf("%s\n", strchr("azertyuiop", 'y'));
+  printf("%s\n", strchr("azertyuiop", 'u'));
+  printf("%s\n", strchr("azertyuiop", 'i'));
+  printf("%s\n", strchr("azertyuiop", 'o'));
+  printf("%s\n", strchr("azertyuiop", 'p'));
+  return 0;
+}
