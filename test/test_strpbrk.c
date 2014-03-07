0161225b1a19d3af64bc96c88066afcac2fd95df
izissise <hugues.m@hotmail.fr>
2014-03-07 16:22:10 +0100

    strpbrk seem to work

--- test/test_strpbrk.c
+++ test/test_strpbrk.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
   printf("toto => %s\n", strpbrk("toto", ""));
   printf(" => %s\n", strpbrk("", "sdfsdf"));
   printf("abcdefghijkl => %s\n", strpbrk("abcdefghijkl", "ejc"));
-  printf("abcdefghijkl => %s\n", strpbrk("abcdefghijkl", "ej"));
+  printf("abcdefghijkl => %s\n", strpbrk("abcdefghijkl", "j"));
 
   return 0;
 }
